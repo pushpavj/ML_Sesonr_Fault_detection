646e2c3d6c6c86a5e2b5e0fa5f18b82a37567dca
PushpaVJ <pushpa_vj@rediffmail.com>
2023-01-08 00:16:15 +0530

    training pipeline configuration code update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from sensor.configuration.mongodb_conection import MongoDBClient
+from sensor.exceptions import SensorException
+import os, sys
+from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 
 if __name__=='__main__':
-    mongdb_client=MongoDBClient()
-    print(mongdb_client.database.list_collection_names())
+
+    training_pipeline_config=TrainingPipelineConfig()
+    data_ingestion_config=DataIngestionConfig(training_pipeline_config)
+    print(data_ingestion_config.__dict__)
+
+    # mongdb_client=MongoDBClient()
+    # print(mongdb_client.database.list_collection_names())
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_requirments()->List[str]:
 
 setup(
     name="sensor",
-    version="0.0.1",
+    version="0.0.2",
     author="Pushpa",
     author_email="pushpapraketh@gmail.com",
     packages=find_packages(),
