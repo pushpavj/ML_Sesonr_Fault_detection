86921cf2910a418e42089abfd66623c87f1c88be
PushpaVJ <pushpa_vj@rediffmail.com>
2023-01-10 02:08:06 +0530

    Constants and configuration details updated

--- sensor/constants/pipeline_constant.py
+++ sensor/constants/pipeline_constant.py
@@ -28,3 +28,50 @@ DATA_VALIDATION_DIR_NAME:str="data_validation"
 DATA_VALIDATION_SCHEMA_DIR='schema.yaml'
 DATA_VALIDATION_VALIDATED_DIR:str='validateddata'
 DATA_VALIDATION_DRIFT_DIR:str="data_drift_reports"
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_VALID_DIR: str = "validated"
+
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
+"""
+MODEL Evauation related constant start with MODEL_EVALUATION var name
+"""
+
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+
+MODEL_PUSHER_BUCKET_NAME = TRAINING_BUCKET_NAME
+
+MODEL_PUSHER_S3_KEY = "model-registry"
